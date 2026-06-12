86caeb820be576b05382ba5f51ab51caf0c99aab
Tien Huy <nanithe123456@gmail.com>
2026-06-12 09:45:24 +0700

    trang dang nhap

---
+++ Project/Login.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class Login : Form
+    {
+
+        public Login()
+        {
+            InitializeComponent();
+            
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text1 = textBox1.Text;
+            string text2 = textBox2.Text;
+            if (text1 == "nanithe123456@gmail.com" && text2 == "0014268")
+            {
+                MessageBox.Show("Đăng nhập thành công!");
+                Form qly = new Quanly();
+                qly.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại!");
+            }
+        }
+    }
+}
